dcaa56e491a37b445e9b08003595f2e7662b1da0
jhhsu8 <jhhsu11@gmail.com>
2020-03-27 12:03:26 -0700

    Create t_test_acute_hargreaves.R

---
+++ t_test_acute_hargreaves.R
@@ -0,0 +1,20 @@
+#t-test for determining whether the wild-type and mutant means are equal to each other
+
+library(ggplot2)
+rm(list = ls(all.names = TRUE)) # clears all environment variables to avoid leaking state to another script
+
+# read data from a csv file
+data <- read.csv("Pain/acute_hargreaves.csv")
+count <- 1
+# loop through data rows from second to eigth columns 
+for (name in colnames(data[, 2:8])) {
+  
+  #Welch’s t-test for unequal variances
+  ttest <- t.test(data$B6N, data[, name])
+  
+  #print p-values and wild-type and mutant means
+  df <- data.frame(row.names=count, wt=colnames(data)[1], mut=name, pvalue=ttest$p.value, wtmean=ttest$estimate[1], mutmean=ttest$estimate[2])
+  print(df)
+  
+  count = count + 1
+}
